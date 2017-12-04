04dce5d22cf51aa1e11698fd7a1265c5faddc94d
NakagamiYuta <guguru0014@gmail.com>
2017-12-04 23:35:35 +0900

    FloatActionButtonを追加

--- app/src/main/java/work/nityc_nyuta/kadaikanrikun/MainActivity.java
+++ app/src/main/java/work/nityc_nyuta/kadaikanrikun/MainActivity.java
@@ -3,7 +3,9 @@ package work.nityc_nyuta.kadaikanrikun;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -13,6 +15,7 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -45,6 +48,24 @@ public class MainActivity extends AppCompatActivity
         //ナビゲーションドロワー設定
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        //FloatngActionButton
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.kadai_add_fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Realm.init(MainActivity.this);
+                Realm realm = Realm.getDefaultInstance();
+                RealmQuery<SubjectDatabase> data = realm.where(SubjectDatabase.class);
+                RealmResults<SubjectDatabase> result = data.findAll();
+                if(result.size() > 0){
+                    Intent intent = new Intent(MainActivity.this, KadaiAddActivity.class);
+                    startActivity(intent);
+                }else{
+                    Toast.makeText(MainActivity.this, "科目を追加してください", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
         setTitle("課題管理くん");
     }
