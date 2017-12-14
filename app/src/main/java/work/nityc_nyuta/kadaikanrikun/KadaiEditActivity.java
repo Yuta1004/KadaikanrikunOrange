b24d75cd965cf33d7a335457f45f79a82a7bde6b
NakagamiYuta <guguru0014@gmail.com>
2017-12-14 15:35:32 +0900

    課題編集アクティビティをとりあえず追加

---
+++ app/src/main/java/work/nityc_nyuta/kadaikanrikun/KadaiEditActivity.java
@@ -0,0 +1,13 @@
+package work.nityc_nyuta.kadaikanrikun;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class KadaiEditActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kadai_edit);
+    }
+}
