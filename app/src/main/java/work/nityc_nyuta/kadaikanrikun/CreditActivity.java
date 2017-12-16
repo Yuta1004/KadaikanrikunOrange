498532ce0ba1f76d368150a67643a33a7dec72ce
NakagamiYuta <guguru0014@gmail.com>
2017-12-16 23:14:38 +0900

    クレジットにメイン画面に戻るオプションメニューを追加

--- app/src/main/java/work/nityc_nyuta/kadaikanrikun/CreditActivity.java
+++ app/src/main/java/work/nityc_nyuta/kadaikanrikun/CreditActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 
@@ -25,5 +27,24 @@ public class CreditActivity extends AppCompatActivity {
         });
 
         setTitle("クレジット");
+    }
+
+    @Override
+    //オプションメニュー作成
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.credit, menu);
+        return true;
+    }
+
+    @Override
+    //オプションメニューが選択された
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if(id == R.id.action_home){
+            finish();
+        }
+
+        return false;
     }
 }
